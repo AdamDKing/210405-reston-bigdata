be9f566b470b2153d862291b7c46173c7c38fbda
Adam King <adamdentonking@gmail.com>
2021-04-08 12:53:00 -0400

    more wordcount implementations

--- week1/wccli/src/main/scala/wccli/Cli.scala
+++ week1/wccli/src/main/scala/wccli/Cli.scala
@@ -3,6 +3,7 @@ package wccli
 import scala.io.StdIn
 import scala.util.matching.Regex
 import java.io.FileNotFoundException
+import scala.collection.mutable.Map
 
 /** CLI class, used to communicate with the user
   */
@@ -88,8 +89,43 @@ class Cli {
   def wordcount(filename: String): Unit = {
     // this line produces an array of "words" that needs to be cleaned up and counted
 
-    FileUtil.getTextContent(filename).split("\\s+")
-    //
+    val words = FileUtil.getTextContent(filename)
+      .replaceAll("\\p{Punct}+", "") //remove all punctuation (i think)
+      .split("\\s+")
+      .map(_.toUpperCase())
+
+    // scala collections have mutable and immutable versions
+    // mutable.Set and immutable.Set, mutable.Map and immutable.Map
+    // exceptions: immutable.List corresponds to mutable.ListBuffer
+    // Array corresponds to mutable.ArrayBuffer
+    val wordcountMap = Map[String, Int]()
+    for(word <- words) {
+      //if word not in map, add it to map with count of 1
+      if(wordcountMap.contains(word)) {
+        wordcountMap.put(word, wordcountMap(word) + 1)
+      } else {
+        //("hello", 44, 44.4, new Car()) a (String, Int, Double, Car) tuple
+        wordcountMap.addOne((word, 1)) // (word, 1) is a (String, Int) tuple
+      }
+    }
+    //foreach when we have tuples is a little strange:
+    // we'll see this often though, so it's good to get familiar with the strangeness
+    // this is how we write a lambda for a tuple:
+    //wordcountMap.foreach({case (word, count) => {println(s"$word: $count")} })
+
+    //wordcountMap.foreachEntry((word:String, count: Int) => {println(s"$word: $count")})
+
+    //alternative, using for loop:
+    for(key <- wordcountMap.keys) {
+      println(s"$key: ${wordcountMap(key)}")
+    }
+
+    //one more alternative, uses a process very similar to Hadoop MapReduce:
+    // we group by identity, so all the words BEAT are grouped together, all the words HEART are grouped together
+    // we map to a key value pair (word, 1), so our HEART group becomes: (HEART,1),(HEART,1),(HEART,1), ...
+    // we reduce the values for each group using _ + _, so we sum all the values together to get (HEART,10)
+    // words.groupMapReduce(identity)(_ => 1)(_ + _)
+    //   .foreach({case (word, count) => {println(s"$word: $count")}})
   }
 
 }
